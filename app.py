fcff4fac0ee60bb04d80f8e1ad43613ddadf87d4
Dylan Salcedo <salcedo.dylan@gmail.com>
2024-10-10 17:17:46 +0800

    test push

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def webhook():
     # You can add authentication if needed test
     if request.method == 'POST':
         # Trigger a git pull
-        repo_path = '.'  # Update this to your repo path
+        repo_path = '.'  # Update this to your repo
         try:
             subprocess.run(['git', '-C', repo_path, 'pull'], check=True)
             subprocess.run(['sudo', 'systemctl', 'restart', 'app'], check=True)
